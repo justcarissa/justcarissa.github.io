456df6f4c6a1196cac0ec987a3ddc088ca9a4c36
Carissa Just <78431891+justcarissa@users.noreply.github.com>
2024-03-02 00:30:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,3 @@
-function toggleMobileMenu(icon) {
-  icon.classList.toggle("open");
-  var mobileMenu = document.querySelector(".mobile-menu");
-  mobileMenu.classList.toggle("active");
-}
-
 document.addEventListener("DOMContentLoaded", function() {
     const sectionsButton = document.querySelector(".sections-button");
     const subMenu = document.querySelector(".sub-menu");
@@ -20,7 +14,17 @@ document.addEventListener("DOMContentLoaded", function() {
             sectionsButton.classList.remove("active");
         }
     });
+
+    // Close the menu when a sub-menu item is clicked
+    const subMenuItems = document.querySelectorAll(".sub-menu li a");
+    subMenuItems.forEach(function(item) {
+        item.addEventListener("click", function() {
+            subMenu.classList.remove("active");
+            sectionsButton.classList.remove("active");
+        });
+    });
 });
+
 
 function handleScroll() {
   var headerHeight = document.getElementById('main-header').offsetHeight;
