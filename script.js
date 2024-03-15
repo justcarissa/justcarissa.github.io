abe20358bcb3ecc54808bfbeb4ac92fdce9b0601
Carissa Just <78431891+justcarissa@users.noreply.github.com>
2024-03-14 23:50:07 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Function to toggle mobile menu
 function toggleMobileMenu(icon) {
   icon.classList.toggle("open");
   var mobileMenu = document.querySelector(".mobile-menu");
@@ -29,8 +30,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
   // Function to close submenu when a link is clicked
   function closeSubMenu() {
-    // Check if screen width is less than or equal to 600px (smaller screens)
-    if (window.innerWidth <= 600) {
+    if (window.innerWidth <= 600) { // Smaller screens
       subMenu.classList.add("hidden");
       hideArrow.style.display = "inline-block";
       openArrow.style.display = "none";
@@ -39,62 +39,73 @@ document.addEventListener("DOMContentLoaded", function() {
     }
   }
 
-  // Add event listeners to hideArrow and openArrow for toggling menu visibility
   hideArrow.addEventListener('click', toggleMenu);
   openArrow.addEventListener('click', toggleMenu);
-
-  // Add event listener to sectionsButton for larger screens
   sectionsButton.addEventListener("click", toggleSubMenu);
 
-  // Add event listener to submenu links to close the menu on smaller screens
   const submenuLinks = document.querySelectorAll('.sub-menu a');
   submenuLinks.forEach(link => {
     link.addEventListener('click', closeSubMenu);
   });
 
-  // Call handleScroll function on DOMContentLoaded and when scrolling
+
+  var allImages = document.querySelectorAll('.first-section-img, .graphicdesign-img, .scrollable-image img, .vertical-scrollable-image img');
+
+  allImages.forEach(function(image) {
+    image.addEventListener('click', function() {
+      var imageContainer = event.target.closest('.image-container, .scrollable-image-container');
+      var image = imageContainer.querySelector('img');
+      var caption = imageContainer.querySelector('.caption');
+
+
+
+      modalImg.src = this.src;
+      var caption = this.nextElementSibling;
+      captionText.innerHTML = caption ? caption.innerHTML : '';
+      modal.style.display = "block";
+    });
+  });
+
+  var span = document.getElementsByClassName("close")[0];
+  span.onclick = function() {
+    var modal = document.getElementById("myModal");
+    modal.style.display = "none";
+  };
+
+  function handleScroll() {
+    var headerHeight = document.getElementById('main-header').offsetHeight;
+    var sections = document.querySelector('.sections');
+    var buttonContainer = document.querySelector('.button-container');
+
+    if (window.pageYOffset > headerHeight + 50) {
+      sections.classList.add('fixed-sections');
+      buttonContainer.classList.add('fixed-button-container');
+    } else {
+      sections.classList.remove('fixed-sections');
+      buttonContainer.classList.remove('fixed-button-container');
+    }
+  }
+
   handleScroll();
   window.addEventListener('scroll', handleScroll);
 });
 
-// Function to handle scroll
-function handleScroll() {
-  var headerHeight = document.getElementById('main-header').offsetHeight;
-  var sections = document.querySelector('.sections');
-  var buttonContainer = document.querySelector('.button-container');
+// Toggle modal function
+function toggleModal(event) {
+  var modal = document.getElementById("myModal");
+  var modalImg = document.getElementById("img01");
+  var captionText = document.getElementById("modalCaption");
 
-  // Check if scrolled beyond the header height + 50px
-  if (window.pageYOffset > headerHeight + 50) {
-    sections.classList.add('fixed-sections');
-    buttonContainer.classList.add('fixed-button-container');
-  } else {
-    sections.classList.remove('fixed-sections');
-    buttonContainer.classList.remove('fixed-button-container');
-  }
-}
+  var imageContainer = event.target.closest('.image-container, .scrollable-image-container');
+  var image = imageContainer.querySelector('img');
+  var caption = imageContainer.querySelector('.caption');
 
-// Get the modal
-var modal = document.getElementById("myModal");
-var modalImg = document.getElementById("img01");
+  modalImg.src = image.src;
+  captionText.innerHTML = caption ? caption.innerHTML : '';
+  modal.style.display = "block";
 
-// Get all elements within scrollable image containers
-var scrollableImages = document.querySelectorAll('.scrollable-image img');
-var verticalScrollableImages = document.querySelectorAll('.vertical-scrollable-image img');
-
-// Combine all images to include those within scrollable image containers
-var allImages = document.querySelectorAll('.first-section-img, .graphicdesign-img, .scrollable-image img, .vertical-scrollable-image img');
-
-allImages.forEach(function(image) {
-  image.onclick = function(){
-    modal.style.display = "block";
-    modalImg.src = this.src;
-  }
-});
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
+  var span = document.getElementsByClassName("close")[0];
+  span.onclick = function() {
+    modal.style.display = "none";
+  };
 }
